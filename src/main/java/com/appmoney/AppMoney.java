617cbede303d8cff6e9014adfd3c41479bf82f73
Vinicius Isola <visola@hubspot.com>
2015-05-05 19:24:19 -0400

    Basic App Money application

---
+++ src/main/java/com/appmoney/AppMoney.java
@@ -0,0 +1,13 @@
+package com.appmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMoney {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppMoney.class, args);
+  }
+
+}

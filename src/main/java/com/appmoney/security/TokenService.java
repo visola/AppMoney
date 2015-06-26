8e49096972e79e6db56a9248a96a90924851ebe9
Vinicius Isola <viniciusisola@gmail.com>
2015-06-25 22:21:47 -0400

    Recreate authentication from token when possible

--- src/main/java/com/appmoney/security/TokenService.java
+++ src/main/java/com/appmoney/security/TokenService.java
@@ -1,12 +1,12 @@
 package com.appmoney.security;
 
-import java.nio.file.AccessDeniedException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-import org.apache.commons.codec.EncoderException;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,16 +14,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import com.appmoney.model.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class TokenService {
 
-  private static final int TOKEN_EXPIRATION = 10 * 1000;
+  private static final String EMPTY_PASSWORD = "";
+
+  private static final int TOKEN_EXPIRATION = 8 * 3600 * 1000;
 
   private Map<String, Authentication> authenticationPerToken = new HashMap<>();
   private Map<String, AuthenticationResponse> responsePerToken = new HashMap<>();
@@ -37,25 +41,30 @@ public class TokenService {
   @Value("${secret}")
   String secret;
 
-  public Optional<Authentication> validateToken(Optional<String> token) throws AccessDeniedException {
+  public Optional<Authentication> validateToken(Optional<String> token) {
     if (token.isPresent()) {
+      AuthenticationResponse auth = null;
+
+      // Is authentication in memory?
       if (responsePerToken.containsKey(token.get())) {
-        AuthenticationResponse auth = responsePerToken.get(token.get());
-        if (auth.getExpires() >= System.currentTimeMillis()) {
-          return Optional.of(authenticationPerToken.get(token.get()));
-        } else {
-          throw new AccessDeniedException("Authentication expired.");
-        }
+        auth = responsePerToken.get(token.get());
+
+      } else { // If not, try to recreate authentication from token
+        auth = recreateAuthentication(token.get());
+      }
+
+      if (auth != null && auth.getExpires() >= System.currentTimeMillis()) {
+        return Optional.of(authenticationPerToken.get(token.get()));
       }
     }
     return Optional.empty();
   }
 
-  public AuthenticationResponse generateToken(String email) throws JsonProcessingException, EncoderException {
-    return generateToken(email, "");
+  public AuthenticationResponse generateToken(String email) {
+    return generateToken(email, EMPTY_PASSWORD);
   }
 
-  public AuthenticationResponse generateToken(String username, String password) throws JsonProcessingException, EncoderException {
+  public AuthenticationResponse generateToken(String username, String password) {
     Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
 
     Optional<Entry<String, Authentication>> entry = authenticationPerToken
@@ -71,15 +80,45 @@ public class TokenService {
     Map<String, Object> data = new HashMap<>();
     data.put("exp", expiration);
     data.put("usr", user.getUsername());
-    data.put("auth", authentication.getAuthorities());
 
-    String json = objectMapper.writeValueAsString(data);
-    String signature = DigestUtils.sha256Hex(String.format("%s%s", secret, json));
-    String token = String.format("%s-%s", Base64.encodeBase64String(json.getBytes()), Base64.encodeBase64String(signature.getBytes()));
+    // Store authorities as an array of Strings
+    data.put("auth", authentication.getAuthorities()
+        .stream()
+        .map(GrantedAuthority::getAuthority)
+        .collect(Collectors.toList()));
 
-    AuthenticationResponse response = new AuthenticationResponse();
-    response.setToken(token);
-    response.setExpires(expiration);
+    try {
+      String json = objectMapper.writeValueAsString(data);
+      String signature = DigestUtils.sha256Hex(String.format("%s%s", secret, json));
+      String token = String.format("%s-%s", Base64.encodeBase64String(json.getBytes()), Base64.encodeBase64String(signature.getBytes()));
+
+      return storeAuthentication(authentication, expiration, token);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException("Error while generating JSON for token.", e);
+    }
+  }
+
+  private AuthenticationResponse recreateAuthentication(String token) {
+    if (token.indexOf("-") >= 0) {
+      String dataPart = token.split("-")[0];
+      try {
+        JsonNode data = objectMapper.readTree(Base64.decodeBase64(dataPart));
+
+        String email = data.get("usr").asText();
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, EMPTY_PASSWORD));
+
+        long expiration = data.get("exp").asLong();
+
+        return storeAuthentication(authentication, expiration, token);
+      } catch (IOException e) {
+        throw new RuntimeException("Invalid authentication token.");
+      }
+    }
+    throw new RuntimeException("Invalid authentication token.");
+  }
+
+  private AuthenticationResponse storeAuthentication(Authentication authentication, long expiration, String token) {
+    AuthenticationResponse response = new AuthenticationResponse(token, expiration);
 
     responsePerToken.put(token, response);
     authenticationPerToken.put(token, authentication);

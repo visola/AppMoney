b0da8d7d90c4c847d28c22a1e2dda0749db8bd60
Vinicius Isola <viniciusisola@gmail.com>
2015-07-11 15:44:07 -0400

    Add recent transactions on home screen

--- src/main/resources/static/js/view/Home.js
+++ src/main/resources/static/js/view/Home.js
@@ -1,16 +1,31 @@
-define(['view/Base', 'tpl!template/home.html', 'collection/Accounts'], function (BaseView, HomeTemplate, Accounts) {
-  return BaseView.extend({
+define(['view/Base', 'view/transaction/Recent', 'tpl!template/home.html', 'collection/Accounts'],
+    function (BaseView, RecentTransactionsView, HomeTemplate, Accounts) {
+
+  var HomeView = BaseView.extend({
     template: HomeTemplate,
 
     initialize: function () {
       var _this = this;
       this.loading = true;
       this.collection = new Accounts();
+      this.recentTransactionsView = new RecentTransactionsView();
 
       this.collection.fetch().then(function () {
         _this.loading = false;
         _this.render();
       });
     },
+
+    render: function () {
+      HomeView.__super__.render.apply(this, arguments);
+
+      if (!this.loading) {
+        this.recentTransactionsView.render();
+        this.$('#recentTransactions').append(this.recentTransactionsView.$el);
+      }
+      return this;
+    }
   });
+
+  return HomeView;
 });
